fad3d474c1292497d1f86dc9b25ebd0eff3436be
dliao-jpg <d.liao@intervex-asia.com>
2026-08-21 09:01:55 +0200

    Refresh PWA cache for warm-up update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-tracker-v2.4.0";
+const CACHE="training-tracker-v2.5.0";
 const ASSETS=["./","./index.html","./app.css","./plan.js","./app.js","./recommendation.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
